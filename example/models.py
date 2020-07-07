86f4da0cd5ca15f80fc298f758a841c6b386e0de
yeremiaChris <yeremia997@gmail.com>
2020-07-07 18:29:29 +0700

    add models with relationship

---
+++ example/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=20)
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    language = models.ManyToManyField(Language)
+
+    def __str__(self):
+        return self.name
